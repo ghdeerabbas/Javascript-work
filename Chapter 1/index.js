092af6de2e295a92bc07ad335baf3e22f4e870fe
ghdeerabbas <110130882+ghdeerabbas@users.noreply.github.com>
2022-10-09 20:22:35 +0500

    workshop of bootsrap

--- Chapter 1/index.js
+++
@@ -1,6 +0,0 @@
-var score = 44;
-if (score > 80 && score <=100 ){console.log("grade A ");}
-else if  (score > 70 && score <=80 ){console.log("grade B ");}
-else if  (score > 60 && score <=70 ){console.log("grade C ");}
-
-else{console.log ("failled")}
