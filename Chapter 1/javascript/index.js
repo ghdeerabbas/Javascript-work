932db37cce9b35d889e6efb353db9f1e0239431c
ghdeerabbas <110130882+ghdeerabbas@users.noreply.github.com>
2022-10-11 13:09:27 +0500

    javascript intro

---
+++ Chapter 1/javascript/index.js
@@ -0,0 +1,10 @@
+var name2 = ("ghdeer abbas");
+console.log(name2);
+
+function abc() {
+    var name3 =  ("zarq");
+    console.log(name3);
+    // console.log(name2 )
+}
+abc();
+console.log (name3)
