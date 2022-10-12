19effe0e1bd648206f4f0f733a4a4869ae993b2a
ghdeerabbas <110130882+ghdeerabbas@users.noreply.github.com>
2022-10-12 12:22:03 +0500

    js

--- Chapter 1/javascript/index.js
+++ Chapter 1/javascript/index.js
@@ -1,10 +1,3 @@
-var name2 = ("ghdeer abbas");
-console.log(name2);
-
-function abc() {
-    var name3 =  ("zarq");
-    console.log(name3);
-    // console.log(name2 )
-}
-abc();
-console.log (name3)
+var name2 = `ghdeer abbas from district 
+ 'bahawalaghar'`;
+console.log(name2)
