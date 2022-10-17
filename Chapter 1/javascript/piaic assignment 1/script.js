7072b9ee67db09ad7c377133a649524dada8579e
ghdeerabbas <110130882+ghdeerabbas@users.noreply.github.com>
2022-10-17 12:37:33 +0500

    js worlshop

---
+++ Chapter 1/javascript/piaic assignment 1/script.js
@@ -0,0 +1,23 @@
+
+// alert( 'wellcome to js land happy coding')
+// // write in var, let
+// let name = "ghdeer abbas"
+// alert('ghdder abbas kamboh kia hall"zafar iqbal"')
+// alert("myemailexample@gmail.com")
+// document.write("yes! i can write HTML,javascript language")
+// let age = 34;
+// // console.log(alert("age"))
+// let brithyear = "i was born in  2003 ";
+
+// document.write(brithyear)
+
+// let birth = "data type of my decleared variable is numbers";
+// document.write(birth
+// var nam= 
+// `variable name can only contain with $,_underscore and numbers
+// variable start with $2323 , -name and 3fnuuj`;
+// document.write(nam)
+let a  = 3;
+let b = 53;
+let c = a+b;
+console.log(c)
---
+++ Chapter 1/javascript/video 80-onward/styles/script.js
@@ -0,0 +1,27 @@
+// value conversion to boolean part
+let name1 = undefined;
+console.log(name1)
+
+let name2= "hello";
+console.log(name2)
+
+let name3 = null;
+console.log(name3)
+
+let name4 ;
+console.log(name4)
+// value conversion with &&
+
+let ghd = "hello" && 1;
+console.log(ghd);
+let sss= "hello" && false;
+console.log(sss);
+let ddd= "hello" && true && 0;
+console.log(ddd);
+// value conversion with ||
+let name5 = "heelo" || 0;
+console.log(name5);
+let name6 = false || 4;
+console.log(name6);
+let name7 = "car" || 0 || 3 ;
+console.log(name7);
