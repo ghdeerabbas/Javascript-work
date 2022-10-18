9aa0693896ef4a60cce0b3c81a528a2fed9ecff4
ghdeerabbas <110130882+ghdeerabbas@users.noreply.github.com>
2022-10-18 13:15:47 +0500

    assignment 1

--- Chapter 1/javascript/Assignment 1/js/script.js
+++ Chapter 1/javascript/Assignment 1/js/script.js
@@ -5,41 +5,59 @@ document.getElementById("alertString").onclick= function(){
     let fullName = firstName +   "  " + lastName
     alert(fullName);
     let statement = "alert ghdeer abbas kamboh from district bahawalghar";
-    document.getElementById("statement").innerHTML= statement;
-    
-
-}
+        document.getElementById("statement").innerHTML= statement;
+     }
 document.getElementById("clearStatement").onclick= function(){
-    document.getElementById("statement").innerHTML=""
-}
+    document.getElementById("statement").innerHTML=""}
+
 // alertnumber
 document.getElementById("alertNumber").onclick= function(){
     let fullName = 23434;
     alert(fullName);
-    let statemen = "alert34343232";
-    document.getElementById("statement").innerHTML= statemen;
+    let statement = "alert34343232";
+    document.getElementById("statement").innerHTML= statement;}
+    document.getElementById("clearStatement").onclick= function(){
+        document.getElementById("statement").innerHTML=""}
+        // variable name
+        document.getElementById("variableName").onclick= function(){
+            // let fullName ="" 
+            // alert(fullName);
+            let statement ="  <ul>   <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. <code>Eveniet delectus</code> blanditiis officia?</li>   <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet delectus blanditiis officia?</li></ul>";;
+            document.getElementById("outPut").innerHTML= statement;}
+            document.getElementById("clearOutput").onclick= function(){
+                document.getElementById("outPut").innerHTML=""}
+        
+
+    // document.getElementById("statement").innerHTML=""}
+    // variable name
+    // document.getElementById("variableName").onclick= function(){
+    //     let fullName = "<un><l></l></un>";
+    //     alert(fullName);
+    //     let statement = "alert34343232";
+    //     document.getElementById("statement").innerHTML= statement;
+    //     document.getElementById("statement").innerHTML="" ;}
     
 
-}
-document.getElementById("clearStatement").onclick= function(){
-    document.getElementById("statement").innerHTML=""}
-// SUM2NUMERS
-document.getElementById("sum2Numbers").onclick= function(){
-    // let fullName = 23434;
-    // alert(fullName);
-    // let statemen = "alert34343";
+
+
+// // // SUM2NUMERS
+
+// // document.getElementById("sum2Numbers").onclick= function(){
+// //     // let fullName = 23434;
+// //     // alert(fullName);
+// //     // let statemen = "alert34343";
     
-    document.getElementById("statement").innerHTML=" let a = 2 ;let b = 4;let c = a+b "
+// //     document.getElementById("statement").innerHTML=" let a = 2 ;let b = 4;let c = a+b "
     
 
-}
-document.getElementById("clearStatement").onclick= function(){
-    document.getElementById("statement").innerHTML="";
+// // }
+// document.getElementById("clearStatement").onclick= function(){
+//     document.getElementById("statement").innerHTML="";
   
-}
-document.getElementById("sum2Numbers").onclick= function(){
-    document.getElementById("output").innerHTML= 6
+// }
+// document.getElementById("sum2Numbers").onclick= function(){
+//     document.getElementById("output").innerHTML= 6
     
-}
-document.getElementById("clearOutput").onclick= function(){
-    document.getElementById("output").innerHTML=""}
+// }
+// document.getElementById("clearOutput").onclick= function(){
+//     document.getElementById("output").innerHTML=""}
