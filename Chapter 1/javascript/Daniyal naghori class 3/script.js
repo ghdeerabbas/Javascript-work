39f9c8a62a34a59dfab4d0194ec531f74956d69f
ghdeerabbas <110130882+ghdeerabbas@users.noreply.github.com>
2022-10-20 14:27:14 +0500

    basic off javascript

---
+++ Chapter 1/javascript/Daniyal naghori class 3/script.js
@@ -0,0 +1,46 @@
+let name = 3;
+let name1 = 4;
+console.log(name+name1);
+
+
+
+let firstname = "ghdeer abbas";
+let lastname =  "zarq abbas";
+let fullname = firstname +" "+ lastname
+
+
+console.log("thanks!"+fullname+"");
+
+let firstname1 = "ghdeer abbas";
+let lastname2 =  "zarq abbas";
+let fullname3 = firstname1.concat(" "+lastname2)
+console.log(fullname3);
+
+// comparison operatore
+
+let a = 2+2 == 4
+console.log(a);
+
+let b = 2+2 === "4"
+console.log(b);
+
+let c = 2+2 !== 4
+console.log(c);
+
+let d = 2+2 < 4
+console.log(d);
+
+let e = 2+2 > 4
+console.log(e);
+
+// nested if
+
+let country = prompt("From which country you belong");
+
+let age = Number (prompt("what is your age ?"))
+
+if (country === "pakistan"){
+    // console.log('you are valid for ticket')
+    if (age >= 18 ) {console.log("succeccful person for ticket")}
+    else {console.log("age restriction")}
+} else {console.log("invalid country")}
