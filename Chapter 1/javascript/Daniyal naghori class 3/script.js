36ff3487be7cc3821457be11e71ce599005faf98
ghdeerabbas <110130882+ghdeerabbas@users.noreply.github.com>
2022-10-24 14:36:57 +0500

    Update script.js

--- Chapter 1/javascript/Daniyal naghori class 3/script.js
+++ Chapter 1/javascript/Daniyal naghori class 3/script.js
@@ -1,46 +1,65 @@
-let name = 3;
-let name1 = 4;
-console.log(name+name1);
+// let name = 3;
+// let name1 = 4;
+// console.log(name+name1);
 
 
 
-let firstname = "ghdeer abbas";
-let lastname =  "zarq abbas";
-let fullname = firstname +" "+ lastname
+// let firstname = "ghdeer abbas";
+// let lastname =  "zarq abbas";
+// let fullname = firstname +" "+ lastname
 
 
-console.log("thanks!"+fullname+"");
+// console.log("thanks!"+fullname+"");
 
-let firstname1 = "ghdeer abbas";
-let lastname2 =  "zarq abbas";
-let fullname3 = firstname1.concat(" "+lastname2)
-console.log(fullname3);
+// let firstname1 = "ghdeer abbas";
+// let lastname2 =  "zarq abbas";
+// let fullname3 = firstname1.concat(" "+lastname2)
+// console.log(fullname3);
 
-// comparison operatore
+// // comparison operatore
 
-let a = 2+2 == 4
-console.log(a);
+// let a = 2+2 == 4
+// console.log(a);
 
-let b = 2+2 === "4"
-console.log(b);
+// let b = 2+2 === "4"
+// console.log(b);
 
-let c = 2+2 !== 4
-console.log(c);
+// let c = 2+2 !== 4
+// console.log(c);
 
-let d = 2+2 < 4
-console.log(d);
+// let d = 2+2 < 4
+// console.log(d);
 
-let e = 2+2 > 4
-console.log(e);
+// let e = 2+2 > 4
+// console.log(e);
 
-// nested if
+// // nested if
 
-let country = prompt("From which country you belong");
+// let country = prompt("From which country you belong");
 
-let age = Number (prompt("what is your age ?"))
+// let age = Number (prompt("what is your age ?"))
 
-if (country === "pakistan"){
-    // console.log('you are valid for ticket')
-    if (age >= 18 ) {console.log("succeccful person for ticket")}
-    else {console.log("age restriction")}
-} else {console.log("invalid country")}
+// if (country.toLowerCase() === "pakistan"){
+//     // console.log('you are valid for ticket')
+//     if (age >= 18 ) {console.log("succeccful person for ticket")}
+//     else {console.log("age restriction")}
+// } else {console.log("invalid country
+
+
+
+
+// push and pop
+
+// let fruit = prompt("Chose any fruit ");
+
+// let fruits = ["banana","orange","apple"];
+
+// let ghd = fruits.pop() 
+// console.log(ghd)
+
+
+const fruits1 = ["bananaa","orangee","applee"];
+fruits1[1] = "ghdeer"
+console.log(fruits1)
+
+
